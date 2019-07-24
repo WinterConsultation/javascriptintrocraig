f51244af9a035813ac9577591701864335eb300a
codenation <codenation@codenations-MacBook-Pro.local>
2019-07-24 16:08:48 +0100

    update

---
+++ Functions.js
@@ -0,0 +1,72 @@
+/*
+let coffeeIsGrinding = false;
+
+const pressGrindBeans = () => {
+    if (coffeeIsGrinding) {
+        console.log ("stopping the grind");
+        coffeeIsGrinding = false;
+    } else {
+        console.log("Grinding is about to begin");
+        coffeeIsGrinding = true;
+    }
+}
+pressGrindBeans ();
+
+let coffeeList = (size, typeOfDrink) => {
+    console.log(`size ${size}, type of drink ${typeOfDrink}`);
+}
+coffeeList ("Grande", "Mocha");
+
+    const addUp=(num1,num2) => {
+        return num1,num2;
+    }
+    addUp(7,3);
+console.log(addUp(2,5));
+
+    let accnumber = 50449921;
+    const cashWithDrawal=(amount,accnum)
+
+const multiplyByNineFifths=(celsius) => {
+    return celsius * (9/5);
+};
+const getFahrenheit =(celsius) =>{
+    return multiplyByNineFifths(celsius)+32;
+};
+console.log("The temperature is" + getFahrenheit(15)+"°f");
+
+let orderCount=0;
+const pizzaOrder = (size,toppings) => {
+    console.log(`size ${size},toppings ${toppings}`);  
+ orderCount++
+    console.log(`This is your order number ${orderCount}`)
+
+}
+   
+    pizzaOrder("supreme","peparoni-chicken")
+    pizzaOrder("small", "ham-pineapple")
+
+    const sortCode= (number0)
+    const acountNumber= (number1)
+    let pin = (number2)
+    let balance=500
+    let withDrawl=(balance(=>balance))
+
+    sortCode=(number0.length(4))
+    accountNumber=(number)
+
+    if (`${number0} && ${number1} && ${number2} are acurate account can be accessed`)
+    else ("access denied")
+
+
+    */
+
+   let orderCount=0;
+   const sandwichOrder = (size,toppings) => {
+       console.log(`your order number is  ${orderCount},${size},${topping0},${topping1},${topping2},${topping3},${topping4}`); 
+   } 
+    orderCount++
+    
+
+sandwichOrder("large","ham","cheese","letuce","onion","peppers")
+sandwichOrder("small","chichen","pesto","mushroom","letuce","peppers")
+
---
+++ loops.js
@@ -0,0 +1,37 @@
+/*
+for (i=9; i>-1; i--)
+console.log(i);
+*/
+let favouriteFilms =[
+    "Ghostbusters",
+    "The Matrix1-2",
+    "Batman begins",
+    "the Usual Suspects",
+    "Meet Joe Black"
+];
+console.log(favouriteFilms[0])
+console.log(favouriteFilms[1])
+console.log(favouriteFilms[2])
+console.log(favouriteFilms[3])
+console.log(favouriteFilms[4])
+
+for (favouriteFilmsIndex =0; favouriteFilmsIndex < favouriteFilms.length; favouriteFilmsIndex++){
+    console.log(favouriteFilms[favouriteFilmsIndex])
+}
+const filmCheck() =>{
+if (favouriteFilm[0] = "Ghostbusters"){
+    console.log ("true")
+}
+    else {
+        console.log ("False")
+    };
+console.log filmCheck()
+/*
+let cards=["Diamond","spade","Heart","Club"];
+letcurrentcard=""
+while(currentCard !="spade"){
+    console.log(currentCard);
+    currentCard=cards[Math.floor(Math.random()*4]
+}
+console.log(currentCard)
+*/
